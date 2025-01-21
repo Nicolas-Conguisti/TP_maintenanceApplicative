50aec5e2ac23faa46d8f7a6f3419972f08d67196
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2025-01-21 14:53:47 +0100

    clean : refacto (chirurgical, changement de signature)

--- carApp/src/main/java/Application.java
+++ carApp/src/main/java/Application.java
@@ -1,5 +1,5 @@
 public class Application {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Voiture maVoiture = new Voiture("Sedan", "Bleu");
         Conducteur moi = new Conducteur("John", 20);
 
